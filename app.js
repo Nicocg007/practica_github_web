7b7d406fcb31e4331c4e43b0e1fde8a9cb9660e0
Nicocg007 <118559751+Nicocg007@users.noreply.github.com>
2026-06-30 06:25:48 -0500

    archivos originales

---
+++ app.js
@@ -0,0 +1,7 @@
+let empleados = [
+    { id: 1, nombre: "Ana Gómez", departamento: "Desarrollo", salario: 2500, activo: true },
+    { id: 2, nombre: "Carlos Mendoza", departamento: "Diseño", salario: 1800, activo: true },
+    { id: 3, nombre: "Lucía Torres", departamento: "Desarrollo", salario: 3100, activo: false },
+    { id: 4, nombre: "Sergio Rivas", departamento: "Marketing", salario: 1500, activo: true },
+    { id: 5, nombre: "Marina Delgado", departamento: "Diseño", salario: 2200, activo: false }
+];
